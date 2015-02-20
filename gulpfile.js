a4c633979da632f15e649a400caea1624ff635c6
Joonas <joonas.westlin@gmail.com>
2015-02-20 19:24:09 +0900

    Added an error message for invalid login. Added ngMessages module. Nodemon now ignores dist folder.

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,8 @@ var src = {
 		"bower_components/angular/angular.js",
 		"bower_components/angular-route/angular-route.js",
 		"bower_components/ngstorage/ngStorage.js",
-		"bower_components/angular-bootstrap/ui-bootstrap-tpls.js"
+		"bower_components/angular-bootstrap/ui-bootstrap-tpls.js",
+		"bower_components/angular-messages/angular-messages.js"
 		],
 		css : [
 		"bower_components/bootstrap/dist/css/bootstrap.css",
