b76d9f39724aca068eb010260b07c5565b4c67eb
juunas11 <juunas11@users.noreply.github.com>
2015-02-26 15:15:58 +0900

    Client unit test coverage
    Added client unit test coverage with karma-coverage. Note: using v 0.2.5
    as newest (0.2.7) had an issue with its CSS linking in the HTML report.

--- karma.conf.js
+++ karma.conf.js
@@ -31,14 +31,19 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'lib/dist/js/scripts.js' : 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'growl'],
+    reporters: ['progress', 'growl', 'coverage'],
 
+    coverageReporter : {
+      type : 'html',
+      dir: 'client-coverage/'
+    },
 
     // web server port
     port: 9876,
